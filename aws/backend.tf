4d51dab93f16b6eea1721e53b6346458fef78c3c
DarkNote07 <jacobsoulis@yahoo.gr>
2025-06-23 18:10:04 +0000

    CI Update

--- aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "jacob-terraform-state-bucket"
-    key            = "dev/aws/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
