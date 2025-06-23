90c094ad82412e4150648cb2488484deb29d323f
DarkNote07 <jacobsoulis@yahoo.gr>
2025-06-23 15:15:35 +0000

    Add declared variables for dev environment

---
+++ envs/dev/aws/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
